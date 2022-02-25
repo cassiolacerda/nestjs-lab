e57245c4314bb0356b5adf1eaf0f921a01c3c0ea
Cassio Lacerda <697508+cassiolacerda@users.noreply.github.com>
2022-02-25 11:23:58 -0300

    1. API with NestJS #1. Controllers, routing and the module structure

---
+++ src/posts/dto/createPost.dto.ts
@@ -0,0 +1,4 @@
+export default interface CreatePostDto {
+  content: string;
+  title: string;
+}
---
+++ src/posts/dto/updatePost.dto.ts
@@ -0,0 +1,5 @@
+export default interface UpdatePostDto {
+  id: number;
+  content: string;
+  title: string;
+}
