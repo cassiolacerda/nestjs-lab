0825e1665f718e295d901dbbbc6547ff47bfa2c6
Cassio Lacerda <697508+cassiolacerda@users.noreply.github.com>
2022-03-21 13:05:12 -0300

    4. Error handling and data validation - Part 2 : Extending the BaseExceptionFilter Globally (Mode 2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 import * as Joi from '@hapi/joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { DatabaseModule } from './database/database.module';
 import { PostsModule } from './posts/posts.module';
+import ExceptionsLoggerFilter from './utils/exceptions-logger.filter';
 
 @Module({
   imports: [
@@ -26,6 +28,12 @@ import { PostsModule } from './posts/posts.module';
     PostsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionsLoggerFilter,
+    },
+  ],
 })
 export class AppModule {}
