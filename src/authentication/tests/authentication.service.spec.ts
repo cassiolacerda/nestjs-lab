4e2188c1ca07506ba8a483b54f6f124c12d27038
Cassio Lacerda <697508+cassiolacerda@users.noreply.github.com>
2022-04-10 14:58:00 -0300

    8. Writing unit tests - Part 4: Mocking ConfigService and JwtService

--- src/authentication/tests/authentication.service.spec.ts
+++ src/authentication/tests/authentication.service.spec.ts
@@ -1,44 +1,29 @@
 import { Test } from '@nestjs/testing';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import * as Joi from '@hapi/joi';
 
 import { AuthenticationService } from '../authentication.service';
 import { UsersService } from '../../users/users.service';
 import User from '../../users/user.entity';
+import mockedConfigService from '../../utils/mocks/config.service';
+import mockedJwtService from '../../utils/mocks/jwt.service';
 
 describe('The AuthenticationService', () => {
   let authenticationService: AuthenticationService;
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          validationSchema: Joi.object({
-            POSTGRES_HOST: Joi.string().required(),
-            POSTGRES_PORT: Joi.number().required(),
-            POSTGRES_USER: Joi.string().required(),
-            POSTGRES_PASSWORD: Joi.string().required(),
-            POSTGRES_DB: Joi.string().required(),
-            JWT_SECRET: Joi.string().required(),
-            JWT_EXPIRATION_TIME: Joi.string().required(),
-            PORT: Joi.number(),
-          }),
-        }),
-        JwtModule.registerAsync({
-          imports: [ConfigModule],
-          inject: [ConfigService],
-          useFactory: async (configService: ConfigService) => ({
-            secret: configService.get('JWT_SECRET'),
-            signOptions: {
-              expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
-            },
-          }),
-        }),
-      ],
       providers: [
         AuthenticationService,
         UsersService,
+        {
+          provide: ConfigService,
+          useValue: mockedConfigService,
+        },
+        {
+          provide: JwtService,
+          useValue: mockedJwtService,
+        },
         {
           provide: getRepositoryToken(User),
           useValue: {},
