3afaa9fff8c8007351474a32fa7734a14ec438ea
Cassio Lacerda <697508+cassiolacerda@users.noreply.github.com>
2022-04-22 20:35:09 -0300

    10. Uploading public files to Amazon S3 - Part 1 : Connecting to Amazon S3

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+import { config } from 'aws-sdk';
 import { AppModule } from './app.module';
 import { ExcludeNullInterceptor } from './utils/interceptors/exclude-null.interceptor';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ExcludeNullInterceptor());
   app.use(cookieParser());
+
+  const configService = app.get(ConfigService);
+  config.update({
+    accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
+    secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
+    region: configService.get('AWS_REGION'),
+  });
+
   await app.listen(3000);
 }
 bootstrap();
